1d60973a87c99689a88c0fd22207296f119aca05
Ivo Nascimento <iannsp@gmail.com>
2015-01-23 20:34:58 -0200

    add method to the interface

--- src/FollowThePath/Strategy/Strategy.php
+++ src/FollowThePath/Strategy/Strategy.php
@@ -2,4 +2,7 @@
 namespace FollowThePath\Strategy;
 
 interface Strategy{
+    public function __construct(array $fisrtMove);
+    public function move($try=0);
+    public function receiveFeedback($feedback);
 }
